7c822d9ece98c39414be58e2d361f0d4acb21f5f
RafaelGarcia <mgrafaelgarcia.2805@gmail.com>
2024-09-19 12:35:18 -0500

    add gradient component

---
+++ src/components/Gradient/Gradient.tsx
@@ -0,0 +1,10 @@
+// components/GradientBox.tsx
+import styles from './Gradient.module.css';
+
+const Gradient = () => {
+  return (
+    <div className={styles.gradient}></div>
+  );
+};
+
+export default Gradient;
