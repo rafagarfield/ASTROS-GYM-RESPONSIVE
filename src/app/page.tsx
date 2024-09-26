4bac566be89087654dd037e107b9b57518970132
RafaelGarcia <mgrafaelgarcia.2805@gmail.com>
2024-09-26 17:22:16 -0500

    add component apple cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 import Header from "@/components/Header/Header";
 import Program from "@/components/Program/Program";
 import VideoSection from "@/components/VideoSection/VideoSection";
+import { AppleCardsCarouselDemo } from "@/components/features/demo";
 export default function Home() {
   return (
     <div>
       <Header />
       <Program />
       <VideoSection />
+      <AppleCardsCarouselDemo />
       
      
       
